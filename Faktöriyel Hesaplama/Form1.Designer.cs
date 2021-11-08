203dd42fab111e235ec2a3051f9dd70b6600ec5c
Slosher <93607233+emirslosher@users.noreply.github.com>
2021-11-08 19:47:12 +0300

    Add files via upload

---
+++ Faktöriyel Hesaplama/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Faktöriyel_Hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı Girin:";
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(29, 68);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(127, 23);
+            this.txtSayi.TabIndex = 1;
+            this.txtSayi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(193, 68);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 23);
+            this.btnHesapla.TabIndex = 2;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(68, 125);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(0, 15);
+            this.lblSonuc.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 212);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ Faktöriyel Hesaplama/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Faktöriyel_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int sayi, faktoriyel = 1;
+            sayi = Convert.ToInt32(txtSayi.Text);
+            if (sayi<=0)
+            {
+                MessageBox.Show("Lütfen pozitif bir değer girin", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                //faktoriyel hesaplamak için sayı değerine kadr olan sayılar birbirleriyle çarpılır
+                for(int i = 1; i <= sayi; i++)
+                {
+                    faktoriyel = faktoriyel * i;
+                }
+                lblSonuc.Text = sayi + "!=" + faktoriyel;
+            }
+            txtSayi.Clear();
+
+        } 
+           
+    }  
+}
